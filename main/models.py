7923bc99e6757454a4edfceb79345e0049ef47dc
Justin P. <justinpascua1459@gmail.com>
2026-01-13 12:34:45 -0800

    Finish alembic initialization

--- main/models.py
+++ main/models.py
@@ -8,9 +8,9 @@ from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 class Canonical(Base):
     __tablename__ = "canonical_names"
 
-    id: Mapped[int] = mapped_column(primary_key = True, nullable = False)
+    id: Mapped[int] = mapped_column(primary_key = True, autoincrement = True)
     title: Mapped[str] = mapped_column(String(64), unique = True, nullable = False)
-    user_id: Mapped[int] = mapped_column(nullable = False)
+    user_id: Mapped[int] = mapped_column(Integer, nullable = False)
 
     def __repr__(self):
         return f"Canonical(id = {self.id!r}, title = {self.title!r}, user_id = {self.user_id!r})"
@@ -18,8 +18,8 @@ class Canonical(Base):
 class AltName(Base):
     __tablename__ = "alt_names"
 
-    id: Mapped[int] = mapped_column(primary_key = True)
-    title: Mapped[str] = mapped_column(String(64), primary_key = True)
+    id: Mapped[int] = mapped_column(primary_key = True, autoincrement = True)
+    title: Mapped[str] = mapped_column(String(64), unique = True, nullable = False)
     canonical: Mapped[str] = mapped_column(String(64), nullable = False)
     user_id: Mapped[int] = mapped_column(nullable = False)
 
@@ -29,10 +29,10 @@ class AltName(Base):
 class Playlist(Base):
     __tablename__ = "playlists"
 
-    id: Mapped[int] = mapped_column(primary_key = True)
-    playlist_title: Mapped[str] = mapped_column(String(64))
+    id: Mapped[int] = mapped_column(primary_key = True, autoincrement = True)
+    playlist_title: Mapped[str] = mapped_column(String(64), nullable = True)
     link: Mapped[str] = mapped_column(String(64), unique = True, nullable = False)
-    user_id: Mapped[int] = mapped_column(nullable = False)
+    user_id: Mapped[int] = mapped_column(Integer, nullable = False)
     created_at: Mapped[datetime] = mapped_column(DateTime, nullable = False)
 
     def __repr__(self):
@@ -41,10 +41,10 @@ class Playlist(Base):
 class SongLink(Base):
     __tablename__ = "song_links"
 
-    id: Mapped[int] = mapped_column(primary_key = True)
+    id: Mapped[int] = mapped_column(primary_key = True, autoincrement = True)
     song_title: Mapped[str] = mapped_column(String(64), unique = True, nullable = False)
     link: Mapped[str] = mapped_column(String(64), unique = True, nullable = False)
-    user_id: Mapped[int] = mapped_column(nullable = False)
+    user_id: Mapped[int] = mapped_column(Integer, nullable = False)
 
     def __repr__(self):
         return f"Playlist(id = {self.id!r}, song_title = {self.song_title!r}, link = {self.link!r}, user_id = {self.user_id!r})"
@@ -52,8 +52,8 @@ class SongLink(Base):
 class User(Base):
     __tablename__ = "users"
 
-    id: Mapped[int] = mapped_column(primary_key = True)
-    email: Mapped[str] = mapped_column(String(64), primary_key = True, unique = True)
-    username: Mapped[str] = mapped_column(String(64), primary_key = True, unique = True)
+    id: Mapped[int] = mapped_column(primary_key = True, autoincrement = True)
+    email: Mapped[str] = mapped_column(String(64), unique = True, nullable = False)
+    username: Mapped[str] = mapped_column(String(64), unique = True, nullable = False)
     password: Mapped[str] = mapped_column(String(64), nullable = False)
 
