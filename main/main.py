6d86dce20b769deb07a4cc80fb3c94f3aa8fc829
Justin P. <justinpascua1459@gmail.com>
2026-01-16 13:15:55 -0800

    Reorder routes for documentation purposes

--- main/main.py
+++ main/main.py
@@ -8,11 +8,11 @@ from .router import model, playlists, songs, auth, users
 
 app = FastAPI()
 
-app.include_router(model.router)
-app.include_router(playlists.router)
-app.include_router(songs.router)
 app.include_router(auth.router)
 app.include_router(users.router)
+app.include_router(songs.router)
+app.include_router(playlists.router)
+app.include_router(model.router)
 
 @app.get("/")
 async def root():
